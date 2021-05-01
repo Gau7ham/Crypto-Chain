9c5e20a5a80fb906010447a0c74b15eba55a0d3a
gau7ham <gauthammudaliar13@gmail.com>
2021-05-01 23:27:25 +0530

    Completed Proof of Work

--- blockchain.js
+++ blockchain.js
@@ -43,7 +43,7 @@ class Blockchain{
 
             if(hash  !== validatedHash)return false;
 
-            if((lastDifficulty - difficulty) > 1)return false;
+            if(Math.abs(lastDifficulty - difficulty) > 1)return false;
         }
 
         return true;
